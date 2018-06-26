7da71fd25806e8cdf4d700f1484aa8df21f6613a
Saptaparna10 <saptaparna1991@gmail.com>
2018-06-26 14:45:54 -0400

    user registration

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB config
 const db = require("./config/keys").mongoURI;
 
 //connect to mongo db
 mongoose
   .connect(db)
-  .then(() => console.log(`connected!!`))
+  .then(() => console.log(`MongoDB connected!!`))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world"));

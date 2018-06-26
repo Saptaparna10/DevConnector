8fa76c46df4c7308c5c70d7a4910a68f9f874f2e
Saptaparna10 <saptaparna1991@gmail.com>
2018-06-26 18:01:49 -0400

    login functionality

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://sappy:spssbbpp10@ds219181.mlab.com:19181/sappy"
+  mongoURI: "mongodb://sappy:spssbbpp10@ds219181.mlab.com:19181/sappy",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 //Load user model
 const User = require("../../models/User");
@@ -44,5 +47,58 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+// @route GET api/user/login
+// @desc Login user/ Returning JWtoken
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    } else {
+      //check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          //user matched
+          //create jwt payload
+          const payload = { id: user.id, name: user.name, avatar: user.avatar };
+          //sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        } else {
+          return res.status(400).json({ password: "Password incorrect" });
+        }
+      });
+    }
+  });
+});
+
+// @route GET api/user/current
+// @desc Return current user
+// @access Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
